b7dfb82f8dfe44c4aa94c9ed185db77f4aaf6a9f
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-03-14 12:22:12 -0300

    primeiro commit

---
+++ Assets/Scripts/Camera/CameraHandle.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandle : MonoBehaviour {
+
+	private static readonly float PanSpeed = 20f;
+	private static readonly float ZoomSpeedTouch = 0.1f;
+	private static readonly float ZoomSpeedMouse = 0.5f;
+
+	private static readonly float[] BoundX = new float[]{ -10f, 5f };
+	private static readonly float[] BoundZ = new float[]{ -18f, 4f };
+	private static readonly float[] ZoomBounds = new float[]{ 10f, 85f };
+
+	private Camera cam;
+
+    private Vector3 lastPanPosition;
+	private int panFingerId; // Touch mode only
+
+	private bool wasZoomingLastFrame; // Touch mode only
+	private Vector2[] lastZoomPositions;
+
+	void Awake(){
+		cam = GetComponent<Camera> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.touchSupported && Application.platform != RuntimePlatform.WebGLPlayer)
+        {
+            HandleTouch();
+        }else {
+            HandleMouse();
+        }
+	}
+
+    void HandleTouch()
+    {
+        switch (Input.touchCount){
+            case 1: //Panning
+                // if the touch began, capture its position and its finger ID.
+                // Otherwise, if the finger ID of the touch doesn't match, skip it.
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    lastPanPosition = touch.position;
+                    panFingerId = touch.fingerId;
+                }else if(touch.fingerId == panFingerId && touch.phase == TouchPhase.Moved)
+                {
+                    PanCamera(touch.position);
+                }
+            break;
+
+            case 2:
+                Vector2[] newPosition = new Vector2[] { Input.GetTouch(0).position, Input.GetTouch(1).position };
+                if (!wasZoomingLastFrame) {
+                    lastZoomPositions = newPosition;
+                    wasZoomingLastFrame = true;
+                } else {
+                    float newDistance = Vector2.Distance(newPosition[0], newPosition[1]);
+                    float oldDistance = Vector2.Distance(lastZoomPositions[0], lastZoomPositions[1]);
+                    float offset = newDistance - oldDistance;
+
+                    ZoomCamera(offset, ZoomSpeedTouch);
+
+                    lastZoomPositions = newPosition;
+                }
+            break;
+
+            default:
+                wasZoomingLastFrame = false;
+             break;
+        }
+    }
+
+    void HandleMouse()
+    {
+        // ON mouse down, capture it's position
+        // Otherwise, if the mouse is still down, pan the camera.
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastPanPosition = Input.mousePosition;
+        } else if(Input.GetMouseButton(0))
+        {
+            PanCamera(Input.mousePosition);
+        }
+
+        // Check for scrolling to zoom the camera
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        ZoomCamera(scroll, ZoomSpeedMouse);
+    }
+
+    void PanCamera(Vector3 newPanPosition) {
+        // Determine how much to move the camera
+        Vector3 offset = cam.ScreenToViewportPoint(lastPanPosition - newPanPosition);
+        Vector3 move = new Vector3(offset.x * PanSpeed, 0, offset.y * PanSpeed);
+
+        // Perform the movement
+        transform.Translate(move, Space.World);
+
+        // Ensure the camera remains within bounds.
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(transform.position.x, BoundX[0], BoundX[1]);
+        pos.y = Mathf.Clamp(transform.position.y, BoundZ[0], BoundZ[1]);
+        transform.position = pos;
+
+        // Cache the position
+        lastPanPosition = newPanPosition;
+    }
+
+    void ZoomCamera(float offset, float speed)
+    {
+        if (offset == 0) {
+            return;
+        }
+
+        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView - (offset * speed), ZoomBounds[0], ZoomBounds[1] );
+    }
+
+
+}
